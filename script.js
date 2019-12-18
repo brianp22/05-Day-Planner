bfc4245527402e04549ee3bc13f65564cbacca7a
Brian Prickril <bprickril22@gmail.com>
2019-12-17 21:31:12 -0500

    figured out local storage problem

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    //setting time to local and creating format
     var gmtTime = moment();
-    // Get the local version of that date
-    var localTime = gmtTime.local();
-    $("#currentDay").text(localTime)
+    var localTime = gmtTime.local().format("dddd, MMMM Do YYYY, hh:mm:ss a");
+    $("#currentDay").text(localTime);
+
+    function renderStorage() {
+        $("#nine").text(JSON.parse(localStorage.getItem("hour-9")));
+        $("#ten").text(JSON.parse(localStorage.getItem("hour-10")));
+        $("#eleven").text(JSON.parse(localStorage.getItem("hour-11")));
+        $("#twelve").text(JSON.parse(localStorage.getItem("hour-12")));
+        $("#one").text(JSON.parse(localStorage.getItem("hour-1")));
+        $("#two").text(JSON.parse(localStorage.getItem("hour-2")));
+        $("#three").text(JSON.parse(localStorage.getItem("hour-3")));
+        $("#four").text(JSON.parse(localStorage.getItem("hour-4")));
+        $("#five").text(JSON.parse(localStorage.getItem("hour-5")));
+    }
+    renderStorage();
+
+    function colorConditions(element, number) {
+        if (new Date().getHours() > number){
+            element.addClass("past")
+        } else if (new Date().getHours() === number) {
+            element.addclass("current")
+        } else {
+            element.addClass("future")
+        }
+    }
+    colorConditions($("#nine"), 9);
+    colorConditions($("#ten"), 10);
+    colorConditions($("#eleven"), 11);
+    colorConditions($("#twelve"), 12);
+    colorConditions($("#one"), 13);
+    colorConditions($("#two"), 14);
+    colorConditions($("#three"), 15);
+    colorConditions($("#four"), 16);
+    colorConditions($("#five"), 17);
+
+
+
+
+    $(".saveBtn").on("click", function () {
+        var time = $(this).parent().attr("id");
+        var value = $(this).siblings(".text-input").val();
+        localStorage.setItem(time, JSON.stringify(value));
+    });
+
 
 
 
